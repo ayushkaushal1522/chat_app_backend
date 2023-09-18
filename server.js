4dad7b10c80b51eef6154cb16d54359cb3e36678
ayushkaushal1522 <ayushkaushal1522@gmail.com>
2023-09-19 00:21:26 +0530

    social media backend

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats} = require("./datafolder/data")
+const  cors = require("cors");
+const connectDB = require("./config/db");
+const app = express();
+const userRoutes = require("./routes/userRoute")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+dotenv.config();
+app.use(express.json())
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("Api is running")
+})
+
+app.use('/api/user',userRoutes)
+app.use(notFound);
+app.use(errorHandler);
+
+
+const port = process.env.PORTNUMBER || 5000;
+
+app.listen(port,()=>{
+    console.log(`server started at port ${port}`);
+})
